ddf1ce4133e32a5533353526d806f8a2377a58a2
Panta <mstimi27@gmail.com>
2021-11-15 15:06:08 +0200

    send

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+// SmoothScroll
+        var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 2000,
+        offset: 0,
+        easing: 'easeInOutQuart', // Easing pattern to use
+     });
